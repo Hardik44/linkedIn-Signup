b0c4c4f7a25274e579e0ea8b508b57e396a1d5c6
Hardik44 <jhardik4445@gmail.com>
2019-08-03 01:07:49 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SignupDetailsComponent } from './signup-details/signup-details.component';
+import {RouterModule} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import { ThankYouComponent } from './thank-you/thank-you.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignupDetailsComponent,
+    ThankYouComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'signup', component: SignupDetailsComponent},
+      {path: 'thank-you', component:ThankYouComponent},
+    ]),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-details.ts
@@ -0,0 +1,4 @@
+export interface UserDetails {
+  localizedFirstName: string;
+  localizedLastName: string;
+}
